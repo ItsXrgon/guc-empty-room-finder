512fa68615c26e01ac9c76d2dd1501409ef30c62
itsXrgon <ali.koheil@gmail.com>
2025-01-21 23:03:53 +0200

    feat: add suspense

--- src/app/_components/Form.tsx
+++ src/app/_components/Form.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import DaySelect from './form/DaySelect';
 import DetectButton from './form/DetectButton';
 import EndSlotSelect from './form/EndSlotSelect';
 import SlotTimesCheckbox from './form/SlotTimesCheckbox';
 import StartSlotSelect from './form/StartSlotSelect';
+import { Skeleton } from '~/components/ui/skeleton';
 
 export default function Form() {
 	return (
 		<div className="flex flex-col gap-4 p-4">
-			<StartSlotSelect />
-			<EndSlotSelect />
-			<DaySelect />
-			<DetectButton />
-			<SlotTimesCheckbox />
+			<Suspense fallback={<Skeleton className="w-full h-12" />}>
+				<StartSlotSelect />
+			</Suspense>
+			<Suspense fallback={<Skeleton className="w-full h-12" />}>
+				<EndSlotSelect />
+			</Suspense>
+			<Suspense fallback={<Skeleton className="w-full h-12" />}>
+				<DaySelect />
+			</Suspense>
+			<Suspense fallback={<Skeleton className="w-full h-12" />}>
+				<DetectButton />
+			</Suspense>
+			<Suspense fallback={<Skeleton className="w-full h-12" />}>
+				<SlotTimesCheckbox />
+			</Suspense>
 		</div>
 	);
 }
