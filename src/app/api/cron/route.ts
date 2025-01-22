a2b1d500b89e79d6829ba0b6517d817a3fdc2bb6
itsXrgon <ali.koheil@gmail.com>
2025-01-22 22:25:10 +0200

    fix: fix syntax for app api route

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,28 +1,29 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { env } from '~/env';
 import { createCaller } from '~/server/api/root';
 
-export default async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
 	const isVercelCron = request.headers.get('x-vercel-cron') === '1';
 
 	if (!isVercelCron) {
-		return new Response('Unauthorized', { status: 401 });
+		return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
 	}
 
 	const authHeader = request.headers.get('authorization');
 
 	if (authHeader !== `Bearer ${env.CRON_SECRET}`) {
-		return new Response('Unauthorized', {
-			status: 401,
-		});
+		return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
 	}
 
 	// @ts-expect-error Context is ignored as we're just triggering the scraper
 	const caller = createCaller({});
 	try {
 		await caller.scraper.trigger();
-		return new Response('Success', { status: 200 });
+		return NextResponse.json({ message: 'Success' }, { status: 200 });
 	} catch {
-		return new Response('Internal Server Error', { status: 500 });
+		return NextResponse.json(
+			{ message: 'Internal Server Error' },
+			{ status: 500 }
+		);
 	}
 }
