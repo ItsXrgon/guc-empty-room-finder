8140bc628550ce93e9e23b2a8dd17eb0b171be84
itsXrgon <ali.koheil@gmail.com>
2025-02-04 04:08:35 +0200

    feat: preload the rooms to cache in the room input

--- src/app/_components/form/rooms/input/RoomInput.tsx
+++ src/app/_components/form/rooms/input/RoomInput.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Label } from "~/components/ui/label";
+import { api } from "~/trpc/react";
 
 import RoomSelect from "./RoomSelect";
 
 export default function RoomInput() {
+
+	// Preload the rooms to the cache
+	api.room.showAllRooms.useQuery();
+
 	return (
 		<div className="flex flex-col gap-2">
 			<Label variant="md">Select Room</Label>
