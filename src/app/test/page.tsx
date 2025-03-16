69c9413d25dbde2263283aab94e3f6c54defb5d2
itsXrgon <ali.koheil@gmail.com>
2025-03-16 11:50:24 +0200

    test x3

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,38 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
+import { useCallback } from "react";
 import { Button } from "~/components/ui/button";
 
 export default function Home() {
-	useEffect(() => {
-		const handleClick = () => {
-			const now = new Date().getTime();
-
-			setTimeout(() => {
-				if (new Date().getTime() - now < 1500) {
-					window.location.href =
-						"https://zeal-dashboard.com/download";
-				}
-			}, 1000);
-		};
-
-		const link = document.getElementById("app-link");
-		link?.addEventListener("click", handleClick);
-
-		return () => {
-			link?.removeEventListener("click", handleClick);
-		};
+	const openApp = useCallback(() => {
+		window.location.href = "zeal-dashboard.com://home";
+		setTimeout(() => {
+			window.location.href = "https://zeal-dashboard.com/download";
+		}, 1000);
 	}, []);
 
-	return (
-		<a
-			href="zeal-dashboard.com://home"
-			id="app-link"
-			target="_blank"
-			rel="noopener noreferrer"
-			className="w-full"
-		>
-			<Button>Download dashboard app</Button>
-		</a>
-	);
+	return <Button onClick={openApp}>Download dashboard app</Button>;
 }
