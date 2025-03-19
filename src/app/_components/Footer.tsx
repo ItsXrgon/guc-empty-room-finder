4c954c4e7f67cffbfbaae1c1b22858de4df092e2
itsXrgon <ali.koheil@gmail.com>
2025-03-19 03:18:26 +0200

    remove foot

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Label } from "~/components/ui/label";
 
 export default function Footer() {
 	return (
 		<footer className="flex flex-col items-center justify-center pb-1">
-			<Label variant="sm">
+			{/* <Label variant="sm">
 				Made by{" "}
 				<a href="" target="_blank" rel="noreferrer noopener">
 					{" "}
@@ -21,7 +20,7 @@ export default function Footer() {
 				<Label variant="sm" className="text-blue-500">
 					Github repo for nerds
 				</Label>
-			</a>
+			</a> */}
 		</footer>
 	);
 }
